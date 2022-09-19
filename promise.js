20861e59c5cec7abf80ba250e17081f4659a13f9
David-Top <david.topanto@students.unnes.ac.id>
2022-09-19 20:31:51 +0700

    feat: fungsi promise async/await

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const result = await Promise.all([
+    promiseTheaterIXX(),
+    promiseTheaterVGC()
+  ])
+
+  return result.reduce(
+    (totalEmosi, theater) => totalEmosi + theater.filter(({ hasil }) => hasil === emosi).length, 0
+  )
+};
 
 module.exports = {
   promiseOutput,
